4776a9b78cff7c8c8f0181b9e184b9be5d91da3a
Ryan Rowe <c3351551@uon.edu.au>
2020-03-06 17:54:17 +1100

    lab 2

---
+++ temp/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+/*
+int main () {
+    float x1,y1;
+    float x2, y2;
+    float x, y;
+    printf("put in x and y");
+    scanf("%f %f" ,&x1, &y1);
+   printf("put in x and y");
+    scanf("%f %f",&x2, &y2);
+    printf("use x point");
+    scanf("%f",&x);
+   y = ((y2-y1)/(x2-x1)) * (x-x1)+y1;
+
+    printf("%f\n",&y);
+
+    return 0;
+     }
+*/
+
+/*int main () {
+    int a;
+    int b;
+    int q;
+    int r;
+    printf ("Enter an integer:");
+    scanf ("%d",&a);
+    printf ("Enter an integer:");
+    scanf ("%d",&b);
+    if (b==0){
+        printf ("division by zero error");
+   } else
+
+   {
+        q= a/b ;
+        r= a%b ;
+
+
+    printf("%d divided by %d is %d ",a,b,q);
+    printf("and the remainder is %d",r);
+   }
+
+
+
+
+
+}
+*/
+/*
+int main (){
+    float x=1, n;
+    int k=1;
+    printf ("value for n");
+    scanf ("%f", &n);
+    while (k<30){
+    x =(x+(n/x))*0.5;
+    k=k+1;}
+    printf ("%f",x);
+
+
+}
+
+*/
+
+
+int main(){
+    float a,b,c;
+    printf("value for a");
+    scanf ("%f", &a);
+    printf("value for b");
+    scanf ("%f", &b);
+    printf("value for c");
+    scanf ("%f", &c);
+
+    D=b*b
+
+}
